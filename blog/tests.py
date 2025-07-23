caee7ea70d1e689dd9f9f9e236fc2af50aa51ac2
Negar Karimi <Negar>
2025-07-23 16:37:11 +0330

    simple test url

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
+from .models import Postblog
 
-# Create your tests here.
+class BlogPostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='Mobile')
+        self.post1 = Postblog.objects.create(
+            title ='ApplePhone',
+            text = 'The Best',
+            status = Postblog.STATUS_CHOICES[0],
+            author = self.user,
+        )
+
+    def test_post_list_url(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_list_url_by_name(self):
+        response = self.client.get(reverse('posts_list'))
+        self.assertEqual(response.status_code, 200)
