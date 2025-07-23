cd47b7cea9d2ef697d9f405a413c21b266fe4671
Negar Karimi <Negar>
2025-07-23 15:20:00 +0330

    add post dtail

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.Post_list_view, name='posts_list'),
+    path('<int:pk>/', views.post_detail_view, name='post_dtail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,7 @@ from .models import Postblog
 def Post_list_view(request):
     post_list = Postblog.objects.all()
     return render(request, 'blog/post_list.html', {'post_list':post_list})
+
+def post_detail_view(request, pk):
+    post = Postblog.objects.get(pk=pk)
+    return render(request, 'blog/post_dtail.html', {'post': post})
