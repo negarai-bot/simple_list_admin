84d86b85e8989c9c9a887bb42668241958e58c2d
Negar Karimi <Negar>
2025-07-24 19:31:15 +0330

    show just published post, add newpost page

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.Post_list_view, name='posts_list'),
     path('<int:pk>/', views.post_detail_view, name='post_dtail'),
+    path('created/', views.post_created_view, name='post_created',)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Postblog
 
 def Post_list_view(request):
-    post_list = Postblog.objects.all()
+    #post_list = Postblog.objects.all()
+    post_list =Postblog.objects.filter(status='pub')
     return render(request, 'blog/post_list.html', {'post_list':post_list})
 
 def post_detail_view(request, pk):
-    post = Postblog.objects.get(pk=pk)
+    #post = Postblog.objects.get(pk=pk)
+    post =get_object_or_404(Postblog, pk=pk)
     return render(request, 'blog/post_dtail.html', {'post': post})
+
+def post_created_view(request):
+    return render(request, 'blog/post_created.html')
